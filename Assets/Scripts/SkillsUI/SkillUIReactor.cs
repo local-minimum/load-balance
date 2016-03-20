10801f334d484b98db0fef38e1132ad8963665f4
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-03-20 15:19:42 +0100

    Retracting and expanding

---
+++ Assets/Scripts/SkillsUI/SkillUIReactor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SkillUIReactor : MonoBehaviour {
+
+	[SerializeField]
+	UIStatus[] enabledWhen;
+
+	Button button;
+	EventTrigger trigger;
+
+	void Awake() {
+		button = GetComponentInChildren<Button> ();
+		trigger = GetComponentInChildren<EventTrigger> ();
+	}
+
+	void OnEnable() {
+		SkillsUI.OnSkillUIStatusChange += HandleSkillsUIStatus;
+	}
+
+	void OnDisable() {
+		SkillsUI.OnSkillUIStatusChange -= HandleSkillsUIStatus;
+	}
+
+	void HandleSkillsUIStatus (PlayerIdentity player, UIStatus status)
+	{
+		if (player != Player.LocalPlayerIdentity)
+			return;
+
+		for (int i = 0; i < enabledWhen.Length; i++) {
+			if (status == enabledWhen [i]) {
+				if (button)
+					button.interactable = true;
+				if (trigger)
+					trigger.enabled = true;
+				return;
+			}
+		}
+		if (trigger)
+			trigger.enabled = false;
+
+		if (button)
+			button.interactable = false;
+	}
+}
--- Assets/Scripts/SkillsUI/SkillsUI.cs
+++ Assets/Scripts/SkillsUI/SkillsUI.cs
@@ -1,8 +1,78 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+
+public enum UIStatus {Showing, Contracting, Hidden, Extending};
+public delegate void SkillUIStatusEvent(PlayerIdentity player, UIStatus status);
 
 public class SkillsUI : MonoBehaviour {
 
+	public static event SkillUIStatusEvent OnSkillUIStatusChange;
+
 	public QuickHintText hintText;
 
+	[SerializeField]
+	float extendedX;
+
+	[SerializeField]
+	float retractedX;
+
+	[SerializeField]
+	AnimationCurve transition;
+
+	[SerializeField, Range(0.5f, 2f)]
+	float transitionTime = 1f;
+
+	float currentX;
+
+	UIStatus _status = UIStatus.Showing;
+
+	public void HideUI() {
+		StartCoroutine (doTransition (UIStatus.Hidden));
+	}
+
+	public void ShowUI() {
+		StartCoroutine (doTransition (UIStatus.Showing));
+	}
+
+	IEnumerator<WaitForSeconds> doTransition(UIStatus target) {
+		var _transition = UIStatus.Contracting;
+		if (target == UIStatus.Hidden && (_status == UIStatus.Hidden || _status == UIStatus.Contracting))
+			yield break;
+		else if (target == UIStatus.Showing && (_status == UIStatus.Showing || _status == UIStatus.Extending))
+			yield break;
+		else if (target == UIStatus.Hidden) {
+			_transition = UIStatus.Contracting;
+		} else if (target == UIStatus.Showing) {
+			_transition = UIStatus.Extending;
+		} else
+			yield break;
+
+
+		_status = _transition;
+		if (OnSkillUIStatusChange != null)
+			OnSkillUIStatusChange (Player.LocalPlayerIdentity, _status);
+		float startTime = Time.timeSinceLevelLoad;
+		RectTransform rt = transform as RectTransform;
+		Vector2 startPos = rt.anchoredPosition;
+		Vector2 targetPos = rt.anchoredPosition;
+		targetPos.x = target == UIStatus.Showing ? extendedX : retractedX;
+
+		while (_status == _transition) {
+			float duration = Mathf.Clamp01((Time.timeSinceLevelLoad - startTime) / transitionTime);
+			rt.anchoredPosition = Vector2.Lerp(startPos, targetPos, transition.Evaluate(duration));
+			if (duration == 1f) {
+				_status = target;
+				if (OnSkillUIStatusChange != null)
+					OnSkillUIStatusChange (Player.LocalPlayerIdentity, _status);
+				break;
+			}
+			yield return new WaitForSeconds (0.01f);
+		}
+			
+	}
+
+	void Start() {
+		_status = UIStatus.Contracting;
+		ShowUI ();
+	}
 }
