cd5db5bc7e57843dbecbd34f3b9136df29ae7ed0
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-03-20 13:48:04 +0100

    Panels

---
+++ Assets/Scripts/SkillsUI/PanelButton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public delegate void PanelSelect(RectTransform panel);
+
+public class PanelButton : MonoBehaviour {
+
+	[SerializeField] bool startSelected = false;
+	[SerializeField] RectTransform panel;
+
+	Button button;
+
+	static event PanelSelect OnPanelSelect;
+
+	void Awake() {
+		button = GetComponentInChildren<Button> ();
+	}
+
+	void Start() {
+		if (startSelected)
+			Select ();
+	}
+
+	void OnEnable() {
+		OnPanelSelect += HandlePanelSelect;
+	}
+
+	void OnDisable() {
+		OnPanelSelect -= HandlePanelSelect;
+	}
+
+	void HandlePanelSelect (RectTransform panel)
+	{
+		bool selected = panel == this.panel;
+		this.panel.gameObject.SetActive (selected);
+		button.interactable = !selected;
+	}
+
+	public void Select() {
+		if (OnPanelSelect != null)
+			OnPanelSelect (panel);
+	}
+
+}
